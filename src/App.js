ae232efbac8e97f7ae0eb3080196fa720b590ff3
Marcos Pereira <mluiz.pereira@gmail.com>
2022-03-19 12:28:10 -0300

    Add material-ui library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
+      <AccessAlarmIcon />
     </div>
   );
 }
