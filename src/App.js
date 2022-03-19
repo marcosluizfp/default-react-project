047e0529596ad8ad5c290e85b6091d7cf0f9b5cd
Marcos Pereira <mluiz.pereira@gmail.com>
2022-03-19 12:32:13 -0300

    clean material ui testing code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
 
 function App() {
   return (
     <div>
       <h1>Hello World</h1>
-      <AccessAlarmIcon />
     </div>
   );
 }
