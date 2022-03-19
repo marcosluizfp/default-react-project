0fd6c8d05d5847753bc8535ba94fdc1f3617ad90
Marcos Pereira <mluiz.pereira@gmail.com>
2022-03-19 18:45:24 -0300

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <h1>Hello World!</h1>
+      </Container>
+    </ThemeProvider>
   );
 }
 
