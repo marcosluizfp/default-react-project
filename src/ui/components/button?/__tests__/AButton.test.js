56d7509af2e935f688fcf00fadb6a866643d31e5
Marcos Pereira <mluiz.pereira@gmail.com>
2022-03-25 11:40:06 -0300

    Remove Enzyme test library

--- src/ui/components/button?/__tests__/AButton.test.js
+++ src/ui/components/button?/__tests__/AButton.test.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-undef */
-import { shallow } from 'enzyme';
-import React from 'react';
-import AButton from '../AButton';
+// import React from 'react';
+// import AButton from '../AButton';
 
 describe('<AButton />', () => {
   it('expect to match snapshot', () => {
-    expect(shallow(<AButton />)).toMatchSnapshot();
   });
 });
