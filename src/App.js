a8c37e11596a093fc07051e2b5e963920f9cf151
Marcos Pereira <mluiz.pereira@gmail.com>
2022-03-26 15:44:13 -0300

    Add ErrorBoundary component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import ErrorBoundary from './ui/error/ErrorBoundary';
 
 const theme = createTheme();
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <h1>Hello World!</h1>
-      </Container>
+      <ErrorBoundary>
+        <Container component="main" maxWidth="xs">
+          <h1>Hello World!</h1>
+        </Container>
+      </ErrorBoundary>
     </ThemeProvider>
   );
 }
---
+++ src/ui/error/ErrorBoundary.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class ErrorBoundary extends React.Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { error: null, errorInfo: null };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // Catch errors in any components below and re-render with error message
+    this.setState({
+      error,
+      errorInfo,
+    });
+    // You can also log error messages to an error reporting service here
+  }
+
+  render() {
+    const { error, errorInfo } = this.state;
+    if (errorInfo) {
+      // Error path
+      return (
+        <div>
+          <h2>Something went wrong.</h2>
+          <details style={{ whiteSpace: 'pre-wrap' }}>
+            {error && error.toString()}
+            <br />
+            {errorInfo.componentStack}
+          </details>
+        </div>
+      );
+    }
+    // Normally, just render children
+    // eslint-disable-next-line react/destructuring-assignment
+    return this.props.children;
+  }
+}
